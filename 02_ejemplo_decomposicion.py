45f152df53c6c95a146caecc3cdeec45878cb683
Jesus Cruz <bep010795@gmail.com>
2022-03-09 23:43:11 -0600

    Decomposicion

---
+++ 02_ejemplo_decomposicion.py
@@ -0,0 +1,28 @@
+class Automovil:
+  def __init__(self, color, modelo, marca):
+    self.color = color
+    self.modelo = modelo
+    self.marca = marca
+    self._estado = "reposo"
+    self._motor = Motor(cilindros = 4)
+
+  def acelerar(self, movimiento = "despacio"):
+    if movimiento == "rapida":
+      self._motor.inyecta_gasolina(cantidad = 10)
+    else:
+      self._motor.inyecta_gasolina(cantidad = 3)
+    self._estado = "movimiento"
+
+
+class Motor:
+  def __init_(self, cilindros, tipo = "gasolina"):
+    self.cilindros = cilindros
+    self.tipo = tipo
+    self.temperatura = 0
+  
+  def inyecta_gasolina(self, cantidad):
+    pass
+    
+
+if __name__ == "__main__":
+  pass
